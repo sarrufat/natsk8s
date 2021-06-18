7eb1c84ae09d3501e7df7764f00d72757e10e1c4
sarrufat <arrufat.sergi@gmail.com>
2021-06-18 16:08:36 +0200

    Java Listener

---
+++ springboot/src/main/java/io/nats/spring/ListenerApp.java
@@ -0,0 +1,11 @@
+package io.nats.spring;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ListenerApp.class, args);
+    }
+
+}
