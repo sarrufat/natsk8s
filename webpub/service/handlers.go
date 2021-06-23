fee8b7c849e80515eb37af8b73999a0991a1d46d
sarrufat <arrufat.sergi@gmail.com>
2021-06-23 16:39:33 +0200

    Testing vi REST Interface

--- webpub/service/handlers.go
+++ webpub/service/handlers.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/nats-io/nats.go"
 	"github.com/sarrufat/natsk8s/pub/rand"
 	"io"
@@ -19,11 +20,19 @@ type (
 		NumMsg      int    `json:"num_msg"`
 		MessageSize int    `json:"message_size"`
 	}
+	NatsTestResponse struct {
+		Status  int    `json:"status"`
+		Message string `json:"message"`
+	}
 )
 
 func (nt *NatsTestRequest) FromJSON(r io.Reader) error {
 	dec := json.NewDecoder(r)
 	return dec.Decode(nt)
+}
+func (nr *NatsTestResponse) ToJSON(w io.Writer) error {
+	enc := json.NewEncoder(w)
+	return enc.Encode(nr)
 }
 func (mp *MessageProducer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 
@@ -35,18 +44,22 @@ func (mp *MessageProducer) ServeHTTP(writer http.ResponseWriter, request *http.R
 			http.Error(writer, "Unable to marshal json", http.StatusInternalServerError)
 			mp.logger.Print(err)
 		}
-		d, _ := json.Marshal(natsTest)
-		mp.logger.Print(string(d))
-		mp.cli.Do(natsTest.Prepare())
+		outcome := mp.cli.Do(natsTest.Prepare())
+		resp := &NatsTestResponse{
+			Status:  http.StatusOK,
+			Message: outcome,
+		}
+		resp.ToJSON(writer)
 	}
 }
 
 func (nt *NatsTestRequest) Prepare() NatsAction {
-	return func(conn *nats.Conn) {
+	return func(conn *nats.Conn) string {
 		for cnt := 0; cnt < nt.NumMsg; cnt++ {
 			data := rand.String(nt.MessageSize)
 			conn.Publish(nt.Subject, []byte(data))
 		}
+		return fmt.Sprintf("%d messages of %d sent", nt.NumMsg, nt.MessageSize)
 	}
 }
 func NewMessageProducer(logger *log.Logger, url string) *MessageProducer {
--- webpub/service/nats.go
+++ webpub/service/nats.go
@@ -10,15 +10,16 @@ type (
 	NatsClient struct {
 		url string
 	}
-	NatsAction func(conn *nats.Conn)
+	NatsAction func(conn *nats.Conn) string
 )
 
-func (cli *NatsClient) Do(action NatsAction) {
+func (cli *NatsClient) Do(action NatsAction) string {
 	nc, err := nats.Connect(cli.url, nats.Name("webpub"), nats.Timeout(5*time.Second))
 	defer nc.Close()
 	if err != nil {
 		log.Fatal(err)
 	} else {
-		action(nc)
+		return action(nc)
 	}
+	return ""
 }
